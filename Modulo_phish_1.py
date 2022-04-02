ca3148d76c17bf385b9e7b242c27e3adfff61335
Abhay-N-J <94858232+Abhay-N-J@users.noreply.github.com>
2022-04-02 14:36:10 +0530

    Add files via upload

---
+++ Modulo_phish_1.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+tb_ele = '//*[@id="shorturl"]'
+ans_ele = '//*[@id="r"]/tbody/tr/td[2]/a[1]'
+b_ele = '//*[@id="w"]/input'
+url = 'https://urlex.org'
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches',['enable-logging'])
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+inp = 'bit.ly/ShunyaJackpot'
+def answer(driver,inp,element):
+    answer = ['bit.ly/ShunyaJackpot']
+    que = driver.find_elements(By.XPATH,element)
+    for a,q in zip(answer,que):
+        q.send_keys(a)
+    return driver
+def submit(driver,element):
+    driver.find_element(By.XPATH,element).click()
+    return driver
+
+driver.get(url)
+# driver.maximize_window()
+driver = answer(driver,inp,tb_ele)
+driver = submit(driver,b_ele)
+
+var = driver.find_element(By.XPATH,ans_ele)
+var2 = var.get_attribute('href')
+print(var2)
+driver.close()
