8f7ba3c1a65ffb28145fae37c63a6b5545c7a8d8
crazydj8 <92433291+crazydj8@users.noreply.github.com>
2022-04-02 11:36:50 +0530

    Add files via upload
    This file contains the code to make a list of top level domains

---
+++ readurl.py
@@ -0,0 +1,10 @@
+import urllib.request
+
+
+req = urllib.request.urlopen("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+l = []
+for line in req: 
+    l.append(line.decode("utf-8").strip().lower())
+
+print(l)
+    
