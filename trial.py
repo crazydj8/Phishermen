e36c3772748fc51ca2dcb95edda4dbf96a839827
crazydj8 <92433291+crazydj8@users.noreply.github.com>
2022-04-02 12:27:02 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,22 +1,20 @@
-import Modulo_phish as m
+import readurl as m
 
-url =  'http://activate.facebook.fblogins.net/88adbao798283o8298398?login.asp'#input()
+url = 'http://activate.facebook.fblogins.net/88adbao798283o8298398?login.asp'#input()
 def method(url):
-        if url[0:5]=='http:':
-            url = url[7:len(url)]
-        elif url[0:5]=='https':
-            url = url[8:len(url)]
-        url = url.split('/')
-        url_1 = url[0].split('.')
-        tld = url_1[-1]
-        return tld
+        d = url.split("://")
+        d = d[1].split("/")
+        d1 = d[0].split('.')
+        return(d1)
+        
 tld = method(url)
 def check(tld):
     for i in m.l:
         if i==tld:
             return True
     return False
-if check(tld):
+
+if check(tld[-1]):
     print("legit")
 else:
     print("Not legit")
